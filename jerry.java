7d85ebd27f49845f6b628ef707bc76e4c5614b65
irisssa <iris.ma@student.tdsb.on.ca>
2022-12-20 17:19:40 -0500

    iris's contributions :)

--- jerry.java
+++ jerry.java
@@ -2,9 +2,24 @@ import java.util.*;
 import java.io.*;
 
 public class jerry {
+    static String genChars() {
+        char[] alphabet = new char[26];
+        for (int i = 65; i < 91; i++) {
+            alphabet[i-65] = (char) i;
+        }
+        String res = "";
+        int a = (int) Math.random()*26;
+        for (int i=0; i<100; i++) {
+            res += alphabet[a];
+            a = (int) (Math.random() * 26);
+        }
+        return res;
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+        long start = System.nanoTime();
+        System.out.println(genChars());
+        long end = System.nanoTime();
     }
     static String readLine(BufferedReader br) throws IOException {
         return br.readLine();
