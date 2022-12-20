4c4bdf884bf0aa8c8693da1415c7e81f43666c68
Greb-the-awesome #2 <gyourselfolden@gmail.com>
2022-12-20 17:06:20 -0500

    bufferedreader stuff

---
+++ jerry.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class jerry {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+    static String readLine(BufferedReader br) throws IOException {
+        return br.readLine();
+    }
+}
