3bf406a102ea52110a9acc39fe1f0fe9a813ea00
Greb-the-awesome #2 <gyourselfolden@gmail.com>
2022-12-20 21:33:05 -0500

    basic character typing test

--- jerry.java
+++ jerry.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class jerry {
+    static int numChars = 10;
     static String genChars() {
         char[] alphabet = new char[26];
         for (int i = 65; i < 91; i++) {
@@ -9,7 +10,7 @@ public class jerry {
         }
         String res = "";
         int a = (int) Math.random()*26;
-        for (int i=0; i<100; i++) {
+        for (int i=0; i<numChars; i++) {
             res += alphabet[a];
             a = (int) (Math.random() * 26);
         }
@@ -17,9 +18,25 @@ public class jerry {
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String chars = genChars();
+        System.out.println(chars);
+        System.out.println("Press enter to start");
+        readLine(br);
         long start = System.nanoTime();
-        System.out.println(genChars());
+        String s = readLine(br);
         long end = System.nanoTime();
+        System.out.println("you took " + String.valueOf(end - start) + " nanoseconds");
+
+        int correct = 0;
+        int wrong = 0;
+        for (int i=0; i<s.length(); i++) {
+            if (s.charAt(i) == chars.charAt(i)) {correct++;}
+            else {wrong++;}
+        }
+        System.out.println("Correct: " + String.valueOf(correct));
+        System.out.println("Wrong: " + String.valueOf(wrong));
+        float percent = (float)wrong / correct;
+        System.out.println("percent wrong: " + String.valueOf(percent));
     }
     static String readLine(BufferedReader br) throws IOException {
         return br.readLine();
